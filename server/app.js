d03af6a77fd81271c05d0b9d1d241f2a70c0536c
josephlarrivy <joseph.larrivy@gmail.com>
2023-06-21 14:59:07 -0500

    first route and classMethod working

--- server/app.js
+++ server/app.js
@@ -1,30 +1,40 @@
 const express = require('express');
-const { ObjectId } = require('mongodb');
 const User = require('./models/user')
 
-const { connectToDb, getDb } = require('./db')
+const { connectToDb } = require('./db')
 
 const app = express()
 app.use(express.json())
 
-let db;
+let user;
 
 connectToDb((err) => {
   if (!err) {
     app.listen(3000, () => {
       console.log('app listening on port 3000')
     })
-    db = getDb()
+    user = new User();
   }
 })
 
-
-
-
-////////////////// routes //////////////////
-
-
 app.get('/testconnection', (req, res) => {
-  res.json({ message: 'connection working' })
+  // res.json({ message: 'connection working' })
+  response = user.testClass()
+  res.json({response})
 })
 
+/////////////////// routes //////////////////
+
+
+app.post('/user', (req, res) => {
+
+  // console.log(req.body)
+
+  user.createUser(req.body)
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({ error: err })
+    })
+})
--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,46 @@
-
-
-
-
-
+const { getDb } = require('../db')
 
 class User {
+  constructor() {
+    this.db = getDb();
+  }
+
+  // Creates a user an adds to the database
+  async createUser(userData) {
+    try {
+      const result = await this.db.collection('users').insertOne(userData);
+      return result;
+    } catch (err) {
+      throw new Error('could not create document');
+    }
+  }
+
+  // Updates the user data for a specific user in the database
+  updateUser(userId, updatedUserData) {
+
+  }
+
+  //Deletes a user from the database based on the given user ID
+  deleteUser(userId) {
+
+  }
+
+  // Retrieves a user from the database based on the given user ID
+  getUser(userId) {
+
+  }
+
+  // Retrieves all users from the database.
+  getUsers() {
+    
+  }
+
+  testClass() {
+    return 'testClass method being called'
+  }
 
 }
 
 
 
-
-
-module.exports = User
+module.exports = User;
