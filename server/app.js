1f04c0cd83f25f61cd8fd84a7ab4fab37b9a396d
josephlarrivy <joseph.larrivy@gmail.com>
2023-06-21 15:50:28 -0500

    all basic CRUD routes working with DB for users

--- server/app.js
+++ server/app.js
@@ -25,16 +25,51 @@ app.get('/testconnection', (req, res) => {
 
 /////////////////// routes //////////////////
 
+app.post('/users', async (req, res) => {
+  try {
+    const result = await user.createUser(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
-app.post('/user', (req, res) => {
+app.patch('/users/:id', async (req, res) => {
+  const { id } = req.params
+  const updates = req.body
+  try {
+    const result = await user.updateUser(id, updates);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+})
 
-  // console.log(req.body)
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const result = await user.deleteUser(id);
+    res.status(202).json(result);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+})
 
-  user.createUser(req.body)
-    .then(result => {
-      res.status(201).json(result)
-    })
-    .catch(err => {
-      res.status(500).json({ error: err })
-    })
+app.get('/user/:username', async (req, res) => {
+  const { username } = req.params
+  try {
+    const result = await user.getUserByUsername(username);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+})
+
+app.get('/users', async (req, res) => {
+  try {
+    const result = await user.getUsers();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 })
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const { getDb } = require('../db')
+const { ObjectId } = require('mongodb');
 
 class User {
   constructor() {
@@ -8,7 +9,8 @@ class User {
   // Creates a user an adds to the database
   async createUser(userData) {
     try {
-      const result = await this.db.collection('users').insertOne(userData);
+      const result = await this.db.collection('users')
+        .insertOne(userData);
       return result;
     } catch (err) {
       throw new Error('could not create document');
@@ -16,24 +18,57 @@ class User {
   }
 
   // Updates the user data for a specific user in the database
-  updateUser(userId, updatedUserData) {
-
+  async updateUser(userId, updatedUserData) {
+    if (ObjectId.isValid(userId)) {
+      try {
+        const result = await this.db.collection('users')
+          .updateOne({ _id: new ObjectId(userId) }, { $set: updatedUserData });
+        return result;
+      } catch (err) {
+        throw new Error('could not update document');
+      }
+    } else {
+      res.status(500).json({ error: 'not a valid document id' })
+    }
   }
 
   //Deletes a user from the database based on the given user ID
-  deleteUser(userId) {
-
+  async deleteUser(userId) {
+    if (ObjectId.isValid(userId)) {
+      try {
+        const result = await this.db.collection('users')
+          .deleteOne({ _id: new ObjectId(userId) });
+        return result;
+      } catch (err) {
+        throw new Error('could not delete document');
+      }
+    } else {
+      res.status(500).json({ error: 'not a valid document id' })
+    }
   }
 
-  // Retrieves a user from the database based on the given user ID
-  getUser(userId) {
-
+  // Retrieves a user from the database based on the given user ID and returns all of their data except for their password
+  async getUserByUsername(username) {
+    try {
+      const result = await this.db.collection('users')
+        .findOne({ username }, { projection: { password: 0 } });
+      return result;
+    } catch (err) {
+      throw new Error('could not retrieve document');
+    }
   }
 
-  // Retrieves all users from the database.
-  getUsers() {
-    
+  // Retrieves all users from the database and returns all of their data except for their password
+  async getUsers() {
+    try {
+      const users = await this.db.collection('users')
+        .find({}, { projection: { password: 0 } }).toArray();
+      return users;
+    } catch (err) {
+      throw new Error('could not retrieve documents');
+    }
   }
+
 
   testClass() {
     return 'testClass method being called'
